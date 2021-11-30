d110f414057a47bfde21c84e5e854ac3eab17e23
sh16ee <sh16ee@gmail.com>
2021-11-30 22:15:28 +0600

    second test commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Finance Appp',
+      title: 'Finance Apps',
       initialRoute: '/auth',
       routes: {
         '/auth': (context) => AuthPage(),
